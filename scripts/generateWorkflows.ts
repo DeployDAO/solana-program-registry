189e06e5a81f5b8878d3a4e5ddc8f6cfda26a113
Ian Macalinao <github@igm.pub>
2021-11-22 12:13:45 -0800

    fix addresses iteration

--- scripts/generateWorkflows.ts
+++ scripts/generateWorkflows.ts
@@ -52,7 +52,7 @@ jobs:
       - name: Publish build to Anchor Registry
         run: |
           cd $(cat dirname)
-          nix shell ../#anchor-${anchorVersion} --command bash -c 'cat addresses.json | jq -r ".programs.mainnet | keys | .[]" | xargs anchor publish --provider.cluster mainnet'
+          nix shell ../#anchor-${anchorVersion} --command bash -c 'cat addresses.json | jq -r ". | keys | .[]" | xargs anchor publish --provider.cluster mainnet'
       - name: Record program artifacts
         run: |
           mkdir artifacts
