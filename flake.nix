93a3a179f6287a6d5eb8a9cdcf60e78aa05ff15f
Kiwi Pepper <kiwipepper@users.noreply.github.com>
2021-12-19 16:11:44 -0800

    Add Anchor 0.19

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     saber-overlay.url = "github:saber-hq/saber-overlay";
-    saber-overlay.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -15,10 +14,8 @@
       "x86_64-darwin"
     ] (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ saber-overlay.overlay ];
-        };
+        pkgs = import nixpkgs { inherit system; }
+          // saber-overlay.packages.${system};
         devShell = pkgs.buildEnv {
           name = "dev-shell";
           paths = with pkgs; [ yj jq bash findutils coreutils ];
@@ -26,6 +23,7 @@
       in {
         packages.anchor-0_17_0 = pkgs.anchor-0_17_0;
         packages.anchor-0_18_0 = pkgs.anchor-0_18_0;
+        packages.anchor-0_19_0 = pkgs.anchor-0_19_0;
         packages.devShell = devShell;
 
         devShell = pkgs.mkShell { buildInputs = [ devShell ]; };
