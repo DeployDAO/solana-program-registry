ec5a1867a30ade07867800c4da9a5b8ecea2aa5d
Ian Macalinao <github@igm.pub>
2021-12-22 17:39:23 -0800

    Adds programs index

--- scripts/generateIndex.ts
+++ scripts/generateIndex.ts
@@ -2,6 +2,7 @@ import type { Idl } from "@project-serum/anchor";
 import type { AxiosError } from "axios";
 import axios from "axios";
 import * as fs from "fs/promises";
+import { startCase } from "lodash";
 import invariant from "tiny-invariant";
 import { parse } from "yaml";
 
@@ -25,11 +26,24 @@ const generateIndex = async () => {
     invariant(lastTag, `no tags for ${repo}`);
     return [repo, lastTag] as const;
   });
+
+  const programs: {
+    label: string;
+    name: string;
+    repo: string;
+    tag: string;
+    address: string;
+    shasum: string;
+  }[] = [];
+
   for (const [repo, tag] of lastTags) {
     const slug = `${repo.replace("/", "__")}-${tag}`;
     try {
       const { data: addresses } = await axios.get<Record<string, string>>(
         buildURL({ slug, file: "addresses.json" })
+      );
+      const { data: checksums } = await axios.get<Record<string, string>>(
+        buildURL({ slug, file: "checksums.json" })
       );
       for (const [programName, address] of Object.entries(addresses)) {
         const { data: idl } = await axios.get<Idl>(
@@ -39,6 +53,27 @@ const generateIndex = async () => {
           `${indexDir}idls/${address}.json`,
           JSON.stringify(idl)
         );
+
+        const [org, repoName] = repo.split("/");
+        if (!org || !repoName) {
+          throw new Error(`invalid repo format: ${repo}`);
+        }
+        const shasum = Object.entries(checksums).find(
+          ([_, fileName]) =>
+            fileName === `artifacts/verifiable/${programName}.so`
+        )?.[0];
+        if (!shasum) {
+          throw new Error(`shasum not found for program: ${repo} ${tag}`);
+        }
+
+        programs.push({
+          label: `${startCase(org)} - ${startCase(programName)}`,
+          name: programName,
+          repo,
+          tag,
+          address,
+          shasum,
+        });
       }
     } catch (e) {
       if ((e as AxiosError).response?.status !== 404) {
@@ -47,6 +82,8 @@ const generateIndex = async () => {
       console.warn(`Could not find idl for ${repo} ${tag}`);
     }
   }
+
+  await fs.writeFile(`${indexDir}programs.json`, JSON.stringify(programs));
 
   const allTags = Object.entries(programsList).flatMap(([repo, tags]) =>
     tags.map((tag) => [repo, tag] as const)
