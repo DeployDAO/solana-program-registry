b510ae84f25d8aab8120cb3724c7d1b4b0a8132e
Ian Macalinao <github@igm.pub>
2022-03-22 16:09:41 -0500

    add anchor 0.23 to flake

--- flake.nix
+++ flake.nix
@@ -12,29 +12,32 @@
       "aarch64-darwin"
       "x86_64-linux"
       "x86_64-darwin"
-    ] (system:
-      let
-        pkgs = import nixpkgs { inherit system; }
-          // saber-overlay.packages.${system};
-        devShell = pkgs.buildEnv {
-          name = "dev-shell";
-          paths = with pkgs; [ yj jq bash findutils coreutils ];
-        };
-      in {
-        packages = with pkgs; {
-          inherit devShell;
-          anchor-0_12_0 = pkgs.anchor-0_12_0;
-          anchor-0_13_2 = pkgs.anchor-0_13_2;
-          anchor-0_15_0 = pkgs.anchor-0_15_0;
-          anchor-0_16_1 = pkgs.anchor-0_16_1;
-          anchor-0_17_0 = pkgs.anchor-0_17_0;
-          anchor-0_18_2 = pkgs.anchor-0_18_2;
-          anchor-0_19_0 = pkgs.anchor-0_19_0;
-          anchor-0_20_1 = pkgs.anchor-0_20_1;
-          anchor-0_21_0 = pkgs.anchor-0_21_0;
-          anchor-0_22_0 = pkgs.anchor-0_22_0;
-        };
+    ]
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; }
+            // saber-overlay.packages.${system};
+          devShell = pkgs.buildEnv {
+            name = "dev-shell";
+            paths = with pkgs; [ yj jq bash findutils coreutils ];
+          };
+        in
+        {
+          packages = with pkgs; {
+            inherit devShell;
+            anchor-0_12_0 = pkgs.anchor-0_12_0;
+            anchor-0_13_2 = pkgs.anchor-0_13_2;
+            anchor-0_15_0 = pkgs.anchor-0_15_0;
+            anchor-0_16_1 = pkgs.anchor-0_16_1;
+            anchor-0_17_0 = pkgs.anchor-0_17_0;
+            anchor-0_18_2 = pkgs.anchor-0_18_2;
+            anchor-0_19_0 = pkgs.anchor-0_19_0;
+            anchor-0_20_1 = pkgs.anchor-0_20_1;
+            anchor-0_21_0 = pkgs.anchor-0_21_0;
+            anchor-0_22_0 = pkgs.anchor-0_22_0;
+            anchor-0_23_0 = pkgs.anchor-0_23_0;
+          };
 
-        devShell = pkgs.mkShell { buildInputs = [ devShell ]; };
-      });
+          devShell = pkgs.mkShell { buildInputs = [ devShell ]; };
+        });
 }
